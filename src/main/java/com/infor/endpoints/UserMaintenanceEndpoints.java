8665341b2bdc0cb21a448515f15e68b281e79ca1
jaypogi69 <jayoliveros286@gmail.com>
2017-04-28 12:41:11 +0800

    add endpoints

--- src/main/java/com/infor/endpoints/UserMaintenanceEndpoints.java
+++ src/main/java/com/infor/endpoints/UserMaintenanceEndpoints.java
@@ -4,4 +4,5 @@ public interface UserMaintenanceEndpoints {
 	public final static String REGISTER_USER = "http://users-service/registeruserservice";
 	public final static String GET_USERINFO = "http://users-service/getuserinfo";
 	public final static String MODIFY_USER = "http://users-service/modifyuser";
+	public final static String GET_USERS = "http://users-service/getllusers";	
 }
