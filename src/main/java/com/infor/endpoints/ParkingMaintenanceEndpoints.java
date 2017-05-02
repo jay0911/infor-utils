be557b5be1169a6ae1154c1d365e08a41ef998a2
jaypogi69 <jayoliveros286@gmail.com>
2017-05-03 01:52:02 +0800

    infor parking saving baseline not clean

--- src/main/java/com/infor/endpoints/ParkingMaintenanceEndpoints.java
+++ src/main/java/com/infor/endpoints/ParkingMaintenanceEndpoints.java
@@ -3,4 +3,5 @@ package com.infor.endpoints;
 public interface ParkingMaintenanceEndpoints {
 	public final static String GET_PARKINGS = "http://parking-service/selectparking";
 	public final static String DELETE_PARKING = "http://parking-service/deleteparking";
+	public final static String SAVE_PARKING = "http://parking-service/saveparking";
 }
