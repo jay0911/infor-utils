2d52ce491fe37091bde90230f859840a724c7a19
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-05-04 18:01:35 +0800

    parking maintenance user query change

--- src/main/java/com/infor/endpoints/UserMaintenanceEndpoints.java
+++ src/main/java/com/infor/endpoints/UserMaintenanceEndpoints.java
@@ -5,4 +5,5 @@ public interface UserMaintenanceEndpoints {
 	public final static String GET_USERINFO = "http://users-service/getuserinfo";
 	public final static String MODIFY_USER = "http://users-service/modifyuser";
 	public final static String GET_USERS = "http://users-service/getllusers";	
+	public final static String GET_INFORPARKINGUSERS = "http://users-service/getallavailuserforparking";	
 }
