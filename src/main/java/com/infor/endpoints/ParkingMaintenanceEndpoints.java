99367cf0e07c09dc1c58832ad970d4b974886634
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-05-05 22:29:48 +0800

    parking maintenance edit

--- src/main/java/com/infor/endpoints/ParkingMaintenanceEndpoints.java
+++ src/main/java/com/infor/endpoints/ParkingMaintenanceEndpoints.java
@@ -4,4 +4,5 @@ public interface ParkingMaintenanceEndpoints {
 	public final static String GET_PARKINGS = "http://parking-service/selectparking";
 	public final static String DELETE_PARKING = "http://parking-service/deleteparking";
 	public final static String SAVE_PARKING = "http://parking-service/saveparking";
+	public final static String SELECT_PARKINGUSERS = "http://parking-service/selectParkingUsers";
 }
