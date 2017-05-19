55156f7d65d19ae98814c772942d2072cf1c41c2
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-05-19 23:20:40 +0800

    added endpoint for transactions

---
+++ src/main/java/com/infor/endpoints/TransactionsEndpoints.java
@@ -0,0 +1,5 @@
+package com.infor.endpoints;
+
+public interface TransactionsEndpoints {
+	public static final String CHECK_IF_REGISTERED_FOR_PARKING = "http://transaction-service/checkregisteredforparking";
+}
