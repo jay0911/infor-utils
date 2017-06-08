6f888375d81f97e0409571281df9ad19737219b9
jaypogi69 <jayoliveros286@gmail.com>
2017-06-08 22:39:42 +0800

    add url for begin and end transaction

--- src/main/java/com/infor/endpoints/TransactionsEndpoints.java
+++ src/main/java/com/infor/endpoints/TransactionsEndpoints.java
@@ -2,4 +2,6 @@ package com.infor.endpoints;
 
 public interface TransactionsEndpoints {
 	public static final String CHECK_IF_REGISTERED_FOR_PARKING = "http://transaction-service/checkregisteredforparking";
+	public static final String BEGIN_TRANSACTION = "http://transaction-service/begintransaction";
+	public static final String END_TRANSACTION = "http://transaction-service/endtransaction";
 }
