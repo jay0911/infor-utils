da36b24049c9cbb6bc4649d20e0d556b5b58ae17
jaypogi69 <jayoliveros286@gmail.com>
2017-06-29 00:42:54 +0800

    add url slots

--- src/main/java/com/infor/endpoints/SlotsEndpoints.java
+++ src/main/java/com/infor/endpoints/SlotsEndpoints.java
@@ -4,4 +4,7 @@ public interface SlotsEndpoints {
 	public static final String GET_UNASSIGNEDSLOTS = "http://slot-service/getUnassignedSlots";
 	public static final String GET_ASSIGNEDSLOTS = "http://slot-service/getAssignedSlots";
 	public static final String GET_ALLSLOTS = "http://slot-service/getAllSlots";
+	
+	public static final String GET_UNAVAILSLOTS = "http://slot-service/getUnAvailSlot";	
+	public static final String GET_AVAILSLOTS = "http://slot-service/getAvailSlot";
 }
