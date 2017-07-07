dc02f25f6f5b7f253d34e62530aedff09a2ab510
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-07-07 20:22:42 +0800

    revised query for available and occupied slots

--- src/main/java/com/infor/endpoints/SlotsEndpoints.java
+++ src/main/java/com/infor/endpoints/SlotsEndpoints.java
@@ -7,4 +7,6 @@ public interface SlotsEndpoints {
 	
 	public static final String GET_UNAVAILSLOTS = "http://slot-service/getUnAvailSlot";	
 	public static final String GET_AVAILSLOTS = "http://slot-service/getAvailSlot";
+	
+	public static final String GET_CONDITIONALSLOTS = "http://slot-service/getAllSlotsConditional";
 }
